48c5d4f99aba01ae745d4cb6093da129455ddd31
Prabhat Ghimire <prabhat.ghimire2@gmail.com>
2025-01-22 15:43:40 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# KIC Tech Task
+
+Provided is a React Native component (`MyComponent`) which:
+
+- Renders a list of items based on data provided via props
+- Each item can be selected or not selected, and this state is toggled by tapping on the item
+- It also has a search bar which filters the rendered items
+- And a clear button to clear the search
---
+++ src/Components/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+} from "react-native";
+
+const MyComponent = ({ data }) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    setDataSource(data);
+  }, [data]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataSource(data.filter((item) => item.name.includes(searchTerm)));
+    }, 1000);
+  }, [searchTerm]);
+
+  const handleSelect = (item) => {
+    setSelectedItems((currentSelectedItems) => [...currentSelectedItems, item]);
+  };
+
+  const handleClear = () => {
+    inputRef.current.clear();
+  };
+
+  return (
+    <View>
+      <TextInput
+        ref={inputRef}
+        onChangeText={setSearchTerm}
+        value={searchTerm}
+      />
+      <TouchableOpacity onPress={handleClear}>
+        <Text>Clear</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={dataSource}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleSelect(item)}>
+            <Text>{item.name}</Text>
+            <Text>
+              {selectedItems.includes(item) ? "Selected" : "Not selected"}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MyComponent;
