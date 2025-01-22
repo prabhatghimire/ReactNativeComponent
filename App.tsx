3c28b5caa6635ad553600e80f661f5d45988041c
Prabhat Ghimire <prabhat.ghimire2@gmail.com>
2025-01-22 20:32:21 +0545

    fix: unselect item on press if it already selected

--- App.tsx
+++ App.tsx
@@ -10,12 +10,11 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   useColorScheme,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import MyComponent from './src/Components';
+import MyComponent from './src/components';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -43,24 +42,5 @@ function App(): React.JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,54 +1,108 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect, useRef} from 'react';
 import {
   Text,
   View,
   TouchableOpacity,
   FlatList,
   TextInput,
-} from "react-native";
+  StyleSheet,
+} from 'react-native';
 
-const MyComponent = ({ data }) => {
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [dataSource, setDataSource] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+// type definationipt for indivisual item
+interface IndivisualItem {
+  id: string;
+  name: string;
+}
+
+// type defination for component props
+interface MyComponentProps {
+  data: Array<IndivisualItem>;
+}
+
+// debounce function to delay the search
+const debounce = (func: Function, delay: number) => {
+  let timeoutId: NodeJS.Timeout | null = null;
+
+  return (...args: any[]) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+const MyComponent = ({data}: MyComponentProps) => {
+  const [selectedItems, setSelectedItems] = useState<Array<IndivisualItem>>([]);
+  const [dataSource, setDataSource] = useState<Array<IndivisualItem>>([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const inputRef = useRef(null);
+
+  // handleSearch function to filter the data based on search term implemented using debounce
+  const handleSearch = debounce((term: string) => {
+    setDataSource(
+      data.filter(item => item.name.toLowerCase().includes(term.toLowerCase())),
+    );
+  }, 1000);
 
   useEffect(() => {
     setDataSource(data);
   }, [data]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setDataSource(data.filter((item) => item.name.includes(searchTerm)));
+    const timeoutId = setTimeout(() => {
+      setDataSource(data.filter(item => item.name.includes(searchTerm)));
     }, 1000);
+    // need to clear the timeout when the component unmounts
+    // or using handleSearch function can also improve the performance
+    return () => {
+      clearTimeout(timeoutId); // Clear the timeout if `searchTerm` changes before the timeout completes
+    };
   }, [searchTerm]);
 
-  const handleSelect = (item) => {
-    setSelectedItems((currentSelectedItems) => [...currentSelectedItems, item]);
+  const handleSelect = (item: IndivisualItem) => {
+    // if the item is already selected then remove it from the selectedItems array before it was missing
+    if (selectedItems.includes(item)) {
+      setSelectedItems(currentSelectedItems =>
+        currentSelectedItems.filter(selectedItem => selectedItem !== item),
+      );
+      return;
+    }
+    setSelectedItems(currentSelectedItems => [...currentSelectedItems, item]);
   };
 
   const handleClear = () => {
-    inputRef.current.clear();
+    // this code ony clears the input field's value  so it will not trigger the useEffect and unable to rerender the default data
+    // inputRef.current.clear();
+    // this code clears the searchTerm value so it will trigger the useEffect and rerender the default data
+    setSearchTerm('');
   };
 
   return (
-    <View>
-      <TextInput
-        ref={inputRef}
-        onChangeText={setSearchTerm}
-        value={searchTerm}
-      />
-      <TouchableOpacity onPress={handleClear}>
-        <Text>Clear</Text>
-      </TouchableOpacity>
+    <View style={styles.mainContainer}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          ref={inputRef}
+          onChangeText={setSearchTerm}
+          value={searchTerm}
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={handleClear}>
+          <Text style={styles.clearInput}>Clear</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={dataSource}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleSelect(item)}>
-            <Text>{item.name}</Text>
-            <Text>
-              {selectedItems.includes(item) ? "Selected" : "Not selected"}
+        contentContainerStyle={styles.contentListContainer}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => handleSelect(item)}
+            style={styles.contentContainer}>
+            <Text style={styles.contentName}>{item.name}</Text>
+            <Text style={styles.contentSelectedIndicator}>
+              {selectedItems.includes(item) ? 'Selected' : 'Not selected'}
             </Text>
           </TouchableOpacity>
         )}
@@ -58,3 +112,23 @@ const MyComponent = ({ data }) => {
 };
 
 export default MyComponent;
+
+const styles = StyleSheet.create({
+  contentContainer: {flexDirection: 'row', justifyContent: 'space-between'},
+  contentListContainer: {paddingVertical: 20},
+  contentSelectedIndicator: {fontSize: 16, color: 'gray'},
+  contentName: {fontSize: 20},
+  mainContainer: {flex: 1, paddingHorizontal: 10},
+  clearInput: {color: 'red'},
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 10,
+    // margin: 10,
+    flex: 1,
+  },
+});
